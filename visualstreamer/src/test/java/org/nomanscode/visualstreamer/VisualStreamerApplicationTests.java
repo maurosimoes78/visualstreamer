39f1d7f45cc535eae90bf8403a2eab621472c65a
Mauro Simoes <mauro.simoes@glookast.com>
2023-12-03 01:02:25 -0300

    Initial Project files added

---
+++ visualstreamer/src/test/java/org/nomanscode/visualstreamer/VisualStreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nomanscode.visualstreamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualStreamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
