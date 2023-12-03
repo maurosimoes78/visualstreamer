0008f1d70fd2d07220dc84e86a54538cc4eee145
Mauro Simoes <mauro.simoes@glookast.com>
2023-12-03 01:15:01 -0300

    Changed Project structure

---
+++ src/main/java/org/nomanscode/visualstreamer/VisualStreamerApplication.java
@@ -0,0 +1,13 @@
+package org.nomanscode.visualstreamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualStreamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualStreamerApplication.class, args);
+	}
+
+}
